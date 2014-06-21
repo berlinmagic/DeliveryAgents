8ef2ca44c2d862f7f11f8e0fcf5a44e0354117c2
jurre <jurrestender@hotmail.com>
2014-06-21 19:21:16 +0200

    better way to generate jobs

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,12 +1,27 @@
 module Api
   class JobsController < ApplicationController
     def index
-      render json: [
-        { id: 1, client_name: "Client 1", date: Date.today + 2 },
-        { id: 2, client_name: "Client 1", date: Date.today + 3 },
-        { id: 3, client_name: "Client 1", date: Date.today + 3 },
-        { id: 4, client_name: "Client 1", date: Date.today + 4 }
-      ]
+      builder = MockJobBuilder.new
+      jobs = [].tap { |j| 10.times { j << builder.build } }
+      render json: jobs
+    end
+
+    private
+
+    class MockJobBuilder
+      def initialize
+        @index = 0
+      end
+
+      def build
+        @index += 1
+        offset = rand(0.0001) % 0.0002
+        {
+          id: @index, date: DateTime.new + @index,
+          client: { name: "Client #{@index}" },
+          location: { lat: 52.535389 + offset, lon: 13.201322 + offset }
+        }
+      end
     end
   end
 end
